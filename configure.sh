e87730a07ab062552e5dabff58ab03d322905201
try46 <try46@users.noreply.github.com>
2017-08-31 00:54:01 +0900

    t

---
+++ configure.sh
@@ -0,0 +1,1 @@
+./configure --with-features=huge --enable-multibyte --enable-luainterp=dynamic --enable-gpm --enable-cscope --enable-fontset --enable-fail-if-missing --prefix=/usr/local --enable-pythoninterp=dynamic --enable-lpython3interp=dynamic --enable-rubyinterp=dynamic 
